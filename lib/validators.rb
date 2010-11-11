d20362ca08f0b8104ea1afb3b0ce4855e83895ee
Nando Vieira <fnando.vieira@gmail.com>
2010-11-11 10:30:07 -0200

    Refactoring; extract constants to a specific file.

--- lib/validators.rb
+++ lib/validators.rb
@@ -1,37 +1,10 @@
 require "active_record"
-require "validators/ip"
+require "validators/constants"
 require "validators/validates_ip_address"
 require "validators/validates_email_format_of"
 require "validators/validates_url_format_of"
 require "validators/validates_ownership_of"
 
 module Validators
-  EMAIL_FORMAT = /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-
-  # Source: https://github.com/henrik/validates_url_format_of
-  IPv4_PART = /\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]/  # 0-255
-
-  if RUBY_VERSION >= "1.9"
-    URL_FORMAT = %r[
-      \A
-      https?://                                                    # http:// or https://
-      ([^\s:@]+:[^\s:@]*@)?                                        # optional username:pw@
-      ( (([^\W_]+\.)*xn--)?[^\W_]+([-.][^\W_]+)*\.[a-z]{2,6}\.? |  # domain (including Punycode/IDN)...
-          #{IPv4_PART}(\.#{IPv4_PART}){3} )                        # or IPv4
-      (:\d{1,5})?                                                  # optional port
-      ([/?]\S*)?                                                   # optional /whatever or ?whatever
-      \Z
-    ]ixs
-  else
-    URL_FORMAT = %r[
-      \A
-      https?://                                                    # http:// or https://
-      ([^\s:@]+:[^\s:@]*@)?                                        # optional username:pw@
-      ( (([^\W_]+\.)*xn--)?[^\W_]+([-.][^\W_]+)*\.[a-z]{2,6}\.? |  # domain (including Punycode/IDN)...
-          #{IPv4_PART}(\.#{IPv4_PART}){3} )                        # or IPv4
-      (:\d{1,5})?                                                  # optional port
-      ([/?]\S*)?                                                   # optional /whatever or ?whatever
-      \Z
-    ]ixu
-  end
+  autoload :Ip, "validators/ip"
 end
---
+++ lib/validators/constants.rb
@@ -0,0 +1,30 @@
+module Validators
+  EMAIL_FORMAT = /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+
+  # Source: https://github.com/henrik/validates_url_format_of
+  IPv4_PART = /\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]/  # 0-255
+
+  if RUBY_VERSION >= "1.9"
+    URL_FORMAT = %r[
+      \A
+      https?://                                                    # http:// or https://
+      ([^\s:@]+:[^\s:@]*@)?                                        # optional username:pw@
+      ( (([^\W_]+\.)*xn--)?[^\W_]+([-.][^\W_]+)*\.[a-z]{2,6}\.? |  # domain (including Punycode/IDN)...
+          #{IPv4_PART}(\.#{IPv4_PART}){3} )                        # or IPv4
+      (:\d{1,5})?                                                  # optional port
+      ([/?]\S*)?                                                   # optional /whatever or ?whatever
+      \Z
+    ]ixs
+  else
+    URL_FORMAT = %r[
+      \A
+      https?://                                                    # http:// or https://
+      ([^\s:@]+:[^\s:@]*@)?                                        # optional username:pw@
+      ( (([^\W_]+\.)*xn--)?[^\W_]+([-.][^\W_]+)*\.[a-z]{2,6}\.? |  # domain (including Punycode/IDN)...
+          #{IPv4_PART}(\.#{IPv4_PART}){3} )                        # or IPv4
+      (:\d{1,5})?                                                  # optional port
+      ([/?]\S*)?                                                   # optional /whatever or ?whatever
+      \Z
+    ]ixu
+  end
+end
