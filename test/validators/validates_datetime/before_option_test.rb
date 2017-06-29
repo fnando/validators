8232620cd00155be395e6f06b9f55c8318bde3de
Nando Vieira <fnando.vieira@gmail.com>
2017-06-28 22:44:41 -0700

    Avoid failures due to time difference.

--- test/validators/validates_datetime/before_option_test.rb
+++ test/validators/validates_datetime/before_option_test.rb
@@ -4,11 +4,14 @@ class ValidatesDatetimeBeforeOptionTest < Minitest::Test
   let(:user) { User.new }
 
   test "rejects when date is set to after :before option" do
-    User.validates_datetime :registered_at, :before => 1.week.ago
-    user.registered_at = Time.now
+    week_ago = 1.week.ago
+    now = Time.now
+
+    User.validates_datetime :registered_at, :before => week_ago
+    user.registered_at = now
 
     refute user.valid?
-    assert_includes user.errors[:registered_at], "needs to be before #{I18n.l(1.week.ago)}"
+    assert_includes user.errors[:registered_at], "needs to be before #{I18n.l(week_ago)}"
   end
 
   test "accepts when date is set accordingly to the :before option" do
