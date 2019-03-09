1cb4ccbd96fe62200785ed978818acd405ec6a21
Nando Vieira <me@fnando.com>
2019-03-09 13:52:44 -0800

    Bump up version.

--- lib/validators/version.rb
+++ lib/validators/version.rb
@@ -2,9 +2,9 @@
 
 module Validators
   module Version
-    MAJOR = 2
-    MINOR = 8
-    PATCH = 1
+    MAJOR = 3
+    MINOR = 0
+    PATCH = 0
     STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
   end
 end
