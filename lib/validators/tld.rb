7289781dbeeb971c3e897002cf810fe4b872eb00
Nando Vieira <me@fnando.com>
2019-03-09 14:09:44 -0800

    Assign stringified version.

--- lib/validators/tld.rb
+++ lib/validators/tld.rb
@@ -9,7 +9,9 @@ module Validators
     end
 
     def self.host_with_valid_tld?(host)
-      return false if host.to_s.split(".").size == 1
+      host = host.to_s
+
+      return false if host.split(".").size == 1
 
       valid?(host[/\.([^.]+)$/, 1].to_s.downcase)
     end
